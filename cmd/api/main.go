496c9a07bc9ff1d12f3c44f085cba8f255e0c437
ayhonz <janmachacek97@gmail.com>
2024-11-26 17:35:32 +0100

    server scaffold

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"whalebone-assignment/internal/server"
+)
+
 func main() {
-	print("Hello")
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+
+	if err != nil && err != http.ErrServerClosed {
+		panic(fmt.Sprintf("http server error: %s", err))
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,54 @@
 package server
 
-func NewServer() {
-	print("server hello")
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	port int
+}
+
+func NewServer() *http.Server {
+
+	NewServer := Server{
+		port: 8080,
+	}
+
+	return &http.Server{
+		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Handler:      NewServer.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+}
+
+func (s *Server) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /save", s.createUser)
+	mux.HandleFunc("GET /{id}", s.createUser)
+
+	return mux
+}
+
+func (s *Server) createUser(w http.ResponseWriter, r *http.Request) {
+	jsonResp, err := json.Marshal("create user")
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+func (s *Server) getUser(w http.ResponseWriter, r *http.Request) {
+	jsonResp, err := json.Marshal("get user")
+	if err != nil {
+		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
 }
