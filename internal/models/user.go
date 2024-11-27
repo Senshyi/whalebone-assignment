b0818375913acab1adc5030f2989565e1b11189c
ayhonz <janmachacek97@gmail.com>
2024-11-27 12:22:50 +0100

    create and get user with fixed data

--- internal/models/user.go
+++ internal/models/user.go
@@ -19,9 +19,18 @@ type User struct {
 	DateOfBirth time.Time
 }
 
-func (m *UserModel) Insert() error {
+func (m *UserModel) Insert(user User) error {
+	res := m.Db.Create(&user)
+	if res.Error != nil {
+		return res.Error
+	}
 	return nil
 }
 func (m *UserModel) GetOne(id string) (User, error) {
-	return User{}, nil
+	var user User
+	res := m.Db.First(&user, "Id = ?", id)
+	if res.Error != nil {
+		return User{}, res.Error
+	}
+	return user, nil
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,23 +1,40 @@
 package server
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
+	"whalebone-assignment/internal/models"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Server struct {
 	port int
+
+	// db *gorm.DB
+
+	users *models.UserModel
 }
 
 func NewServer() *http.Server {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&models.User{})
+
 	NewServer := Server{
-		port: port,
+		port:  port,
+		users: &models.UserModel{Db: db},
 	}
 
 	return &http.Server{
@@ -33,24 +50,30 @@ func (s *Server) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /save", s.createUser)
-	mux.HandleFunc("GET /{id}", s.createUser)
+	mux.HandleFunc("GET /{id}", s.getUser)
 
 	return mux
 }
 
 func (s *Server) createUser(w http.ResponseWriter, r *http.Request) {
-	jsonResp, err := json.Marshal("create user")
+	parsedTim, err := time.Parse("2006-01-02T15:04:05-07:00", "2020-01-01T12:12:34+00:00")
 	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
+		log.Printf("error parsing time: ", err)
 	}
 
-	_, _ = w.Write(jsonResp)
+	newUser := models.User{ID: "ca3ae13c-3c97-49fc-b4bb-86f04d934142", Name: "Joseph", Email: "email@gmail.com", DateOfBirth: parsedTim}
+	err = s.users.Insert(newUser)
+	if err != nil {
+		log.Printf("unable to create User: %v", err)
+	}
+
+	respondWithJSON(w, http.StatusCreated, struct{}{})
 }
 func (s *Server) getUser(w http.ResponseWriter, r *http.Request) {
-	jsonResp, err := json.Marshal("get user")
+	user, err := s.users.GetOne("ca2ae13c-3c97-49fc-b4bb-86f04d934142")
 	if err != nil {
-		log.Fatalf("error handling JSON marshal. Err: %v", err)
+		log.Printf("unable to get user from db: %v", err)
 	}
 
-	_, _ = w.Write(jsonResp)
+	respondWithJSON(w, http.StatusOK, user)
 }
